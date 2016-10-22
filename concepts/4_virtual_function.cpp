484945d268b0335956260fb9d1874e336901c190
Kuan-Yu Chen <starmap0312@gmail.com>
2016-10-22 15:51:24 +0800

    add some

--- concepts/4_virtual_function.cpp
+++ concepts/4_virtual_function.cpp
@@ -1,23 +1,17 @@
 /* virtual method enables polymorphism
  *   when use a Base class reference to call a virtual method, the concrete implementation of the method will be called
- * - overload function: early binding/static binding, which function to call is 
- *   determined at compile time
- * - override function: function is re-defined in derived classes
- * - virtual function: late binding/dynamic binding, which function to call is
- *   determined at run time
- * - a class with virtual functions is called a polymorphic class, as it embodies
- *   polymorphism, i.e. you can use a Base pointer/reference that points to a
- *   Derived instance to invokes the Derived instance's function at run time
- * - when a base pointer points to a derived object, using the base pointer to invoke
- *   a virtual function the program knows which function to call only at run time
- *   i.e. invokes the derived class's implementation of the virtual function, not
- *   the base class's implementation
- *   on the other hand, if the function is not declared as virtual, the program
- *   invokes the base class's implementation
- * - pure virtual function: a virtual function without implementation in base class
- *   therefore, the base class is an abstract class and cannot be instantiated
- *   moreover, the pure virtual function must be implemented in the derived classes
- *   disobeying the above two rules will lead to compile error
+ * - override method: function is re-defined in derived classes
+ * 1) normal method:
+ *    early binding/static binding: which function to be called is determined at compile time
+ *    i.e. if the function is not declared as virtual, the program invokes the base class's implementation
+ * 2) virtual method:
+ *    late binding/dynamic binding, which function to call is determined at run time
+ *    (a class with virtual functions is called a polymorphic class, as it embodies polymorphism)
+ *    i.e. you can use a Base pointer/reference that points to a Derived instance to invokes the Derived implementation at run time
+ * 3) pure virtual method:
+ *    i)  no implementation in base class, i.e. the base class is an abstract class and cannot be instantiated
+ *    ii) the pure virtual function must be implemented in the derived classes
+ *    (disobeying the above two rules will lead to compile error)
  */
 #include <iostream>
 
